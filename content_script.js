532ec88e7369b5e46b59d490f9b9b5f6af8d2481
fkolcu <furkankolcu@hotmail.com.tr>
2023-02-17 14:33:23 +0300

    use cookie in all subdomains

--- content_script.js
+++ content_script.js
@@ -1,4 +1,6 @@
 document.body.addEventListener('mouseup', function (e) {
     const selectedText = document.getSelection().toString();
-    document.cookie = "fk_cd_ext_st=" + selectedText + ";path=/";
+    document.cookie = "fk_cd_ext_st=" + selectedText + ";domain=." +
+        location.hostname.split('.').reverse()[1] + "." +
+        location.hostname.split('.').reverse()[0] + ";path=/";
 });
