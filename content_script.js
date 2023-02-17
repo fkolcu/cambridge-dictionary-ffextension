2dbfca24120fc18eb2d7a57e30952540db96c6b1
fkolcu <furkankolcu@hotmail.com.tr>
2023-02-17 14:50:21 +0300

    fix bug: forgotten ; corrupts path of cookie

--- content_script.js
+++ content_script.js
@@ -2,5 +2,5 @@ document.body.addEventListener('mouseup', function (e) {
     const selectedText = document.getSelection().toString();
     document.cookie = "fk_cd_ext_st=" + selectedText + ";domain=." +
         location.hostname.split('.').reverse()[1] + "." +
-        location.hostname.split('.').reverse()[0] + ";path=/SameSite=None;Secure=true;";
+        location.hostname.split('.').reverse()[0] + ";path=/;SameSite=None;Secure=true;";
 });
