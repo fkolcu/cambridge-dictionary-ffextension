5e58aeb31cfaed4c50675af1ca98e93b516c2ac6
fkolcu <furkankolcu@hotmail.com.tr>
2023-03-31 17:26:35 +0300

    make selected text url safe so it doesn't corrupt cookie text

--- content_script.js
+++ content_script.js
@@ -1,5 +1,6 @@
 document.body.addEventListener('mouseup', function (e) {
-    const selectedText = document.getSelection().toString();
+    const selectedText = encodeURI(document.getSelection().toString());
+
     document.cookie = "fk_cd_ext_st=" + selectedText + ";domain=." +
         location.hostname.split('.').reverse()[1] + "." +
         location.hostname.split('.').reverse()[0] + ";path=/;SameSite=None;Secure=true;";
