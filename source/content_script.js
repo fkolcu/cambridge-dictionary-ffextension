de88466ce2615a5ff827450e4c95242e7e912d02
fkolcu <furkankolcu@hotmail.com.tr>
2023-08-05 01:18:06 +0300

    quickly recover addon's ability - Closes #2

--- source/content_script.js
+++ source/content_script.js
@@ -1,7 +1,105 @@
 document.body.addEventListener('mouseup', function (e) {
-    const selectedText = encodeURI(document.getSelection().toString());
+    if (location.search.includes('ref=cdext')) {
+        return
+    }
 
-    document.cookie = "fk_cd_ext_st=" + selectedText + ";domain=." +
-        location.hostname.split('.').reverse()[1] + "." +
-        location.hostname.split('.').reverse()[0] + ";path=/;SameSite=None;Secure=true;";
+    let selectedText = document.getSelection().toString().trim()
+    selectedText = encodeURI(selectedText);
+
+    browser.storage.sync.set({
+        selectedWord: selectedText
+    });
 });
+
+const template = `
+<style>
+   html,body{
+    width: 600px;
+    height: 520px;
+   }
+  .cdext_selector_container{
+    position: fixed;
+    width: 100%;
+    height: 51px;
+    border: 1px solid #f0f0f0;
+    z-index: 9999;
+    bottom: 0;
+  }
+  select#cdext_selector {
+    border: none;
+    background-color: #e74c3c;
+    color: white;
+    width: 100%;
+    height: 50px;
+    padding: 10px;
+    cursor: pointer;
+  }
+</style>
+
+<div class="cdext_selector_container">
+  <select id="cdext_selector">
+    <option value="english">English</option>
+    <optgroup label="Bilingual Dictionaries">
+      <option value="english-dutch">English–Dutch</option>
+      <option value="english-french">English–French</option>
+      <option value="english-german">English–German</option>
+      <option value="english-indonesian">English–Indonesian</option>
+      <option value="english-italian">English–Italian</option>
+      <option value="english-japanese">English–Japanese</option>
+      <option value="english-norwegian">English–Norwegian</option>
+      <option value="english-polish">English–Polish</option>
+      <option value="english-portuguese">English–Portuguese</option>
+      <option value="english-spanish">English–Spanish</option>
+    </optgroup>
+    <optgroup label="Semi-bilingual Dictionaries">
+      <option value="english-arabic">English–Arabic</option>
+      <option value="english-catalan">English–Catalan</option>
+      <option value="english-chinese-simplified">English–Chinese (Simplified)</option>
+      <option value="english-chinese-traditional">English–Chinese (Traditional)</option>
+      <option value="english-czech">English–Czech</option>
+      <option value="english-danish">English–Danish</option>
+      <option value="english-hindi">English–Hindi</option>
+      <option value="english-korean">English–Korean</option>
+      <option value="english-malaysian">English–Malay</option>
+      <option value="english-russian">English–Russian</option>
+      <option value="english-thai">English–Thai</option>
+      <option value="english-turkish">English–Turkish</option>
+      <option value="english-ukrainian">English–Ukrainian</option>
+      <option value="english-vietnamese">English–Vietnamese</option>
+    </optgroup>
+  </select>
+</div>
+`;
+
+if (location.search.includes('ref=cdext')) {
+    let storageItem = browser.storage.sync.get('selectedDictionaryKey');
+    storageItem.then((res) => {
+        let selectedDictionary = res.selectedDictionaryKey
+        if (selectedDictionary === undefined) {
+            selectedDictionary = "english"
+        }
+
+        const templateContainer = document.createElement('div');
+        templateContainer.innerHTML = template;
+        document.body.appendChild(templateContainer);
+
+        document.getElementById('cdext_selector').value = selectedDictionary;
+        document.getElementById('cdext_selector').addEventListener('change', onDictionaryChanged)
+    });
+}
+
+function onDictionaryChanged(event) {
+    browser.storage.sync.set({
+        selectedDictionaryName: event.target.innerText, selectedDictionaryKey: event.target.value,
+    });
+
+    let storageItem = browser.storage.sync.get('selectedWord');
+    storageItem.then((res) => {
+        let selectedWord = res.selectedWord
+        if (selectedWord === undefined || selectedWord === "") {
+            return
+        }
+
+        window.location = 'https://dictionary.cambridge.org/dictionary/' + event.target.value + '/' + selectedWord + '?ref=cdext';
+    });
+}
--- source/popup.js
+++ source/popup.js
@@ -1,88 +1,56 @@
-let currentTab = null;
-let currentSelectedWord = null;
-const cookieNameMap = {
-    selectedWord: "fk_cd_ext_st",
-};
+const template = `
+<style>
+html,body{
+    width: 600px;
+    height: 520px;
+}
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+  }
 
-initialize();
+  .container {
+    width: 600px;
+    height: 520px;
+    background-color: #f0f0f0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 3px solid red;
+    padding: 5px;
+  }
 
-function initialize() {
-    const queryTab = browser.tabs.query({active: true});
-    queryTab.then(onQueryTabSuccessful, onQueryTabFailed);
+  .centered-text {
+    text-align: center;
+    font-size: 24px;
+  }
+</style>
 
-    let storageItem = browser.storage.sync.get('selectedDictionaryKey');
+<div class="container">
+  <p class="centered-text">No word selected</p>
+</div>
+`;
+
+let storageItem = browser.storage.sync.get('selectedWord');
+storageItem.then((res) => {
+    let selectedWord = res.selectedWord
+    if (selectedWord === undefined || selectedWord === "") {
+        const templateContainer = document.createElement('div');
+        templateContainer.innerHTML = template;
+        document.getElementById('content').appendChild(templateContainer);
+        return
+    }
+
+    storageItem = browser.storage.sync.get('selectedDictionaryKey');
     storageItem.then((res) => {
         let selectedDictionary = res.selectedDictionaryKey
         if (selectedDictionary === undefined) {
             selectedDictionary = "english"
         }
 
-        document.getElementById('dictionary-selector').value = selectedDictionary;
+        window.location = 'https://dictionary.cambridge.org/dictionary/' + selectedDictionary + '/' + selectedWord + '?ref=cdext';
     });
-}
-
-function onQueryTabSuccessful(tabs) {
-    currentTab = tabs[0];
-
-    readCookie(currentTab, cookieNameMap.selectedWord, function (cookie1) {
-        if (cookie1) {
-            currentSelectedWord = cookie1.value
-            let storageItem = browser.storage.sync.get('selectedDictionaryKey');
-            storageItem.then((res) => {
-                let selectedDictionary = res.selectedDictionaryKey
-                if (selectedDictionary === undefined) {
-                    selectedDictionary = "english"
-                }
-
-                openDictionary(currentSelectedWord, selectedDictionary)
-            });
-        }
-    });
-}
-
-function onQueryTabFailed(error) {
-    console.log('Error happened while querying tabs:', error)
-}
-
-function readCookie(tab, cookieName, callback) {
-    const cookiePromise = browser.cookies.get({
-        url: tab.url,
-        name: cookieName
-    });
-    cookiePromise.then(callback);
-}
-
-function openDictionary(word, dictionary) {
-    let iframe = null
-    let existIframes = document.getElementById('cd_content').getElementsByTagName('iframe')
-    if (existIframes.length > 0) {
-        iframe = existIframes[0]
-    }
-
-    console.log("Dictionary of " + dictionary + " is loading for the word of " + word);
-
-    const url = 'https://dictionary.cambridge.org/dictionary/' + dictionary + '/' + word;
-
-    if (iframe === null) {
-        iframe = document.createElement('iframe');
-    }
-
-    iframe.width = '580';
-    iframe.height = '530';
-    iframe.src = url;
-
-    document.getElementById('cd_content').appendChild(iframe);
-}
-
-// Add listener to dictionary selector
-document.getElementById('dictionary-selector')
-    .addEventListener("change", onDictionarySelected);
-
-function onDictionarySelected(event) {
-    browser.storage.sync.set({
-        selectedDictionaryName: event.target.innerText,
-        selectedDictionaryKey: event.target.value,
-    });
-
-    openDictionary(currentSelectedWord, event.target.value)
-}
+});
